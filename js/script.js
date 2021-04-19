066c3603e752d1b9a7bcaaee2d75b5c16f1d89e2
AgneseBettio <bettio.agnese@gmail.com>
2021-04-19 15:48:11 +0200

    inizio note js per impostare lavoro

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 //Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
 
+//quali imput ho per fare prezzo? name(nome necessario per ordinazione) ingredients (#cheese-add #tomato-add #egg-add #lettuce-add #mustard-add #ketchap-add) coupon (applico % sconto ad arrey codici inventati)
+
 //creo var generali utilizzabili in file - DOM estrapolo elementi
