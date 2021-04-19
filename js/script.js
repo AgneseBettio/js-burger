daa76d1b39e5a6566c185d2e4a65fe62c6d220d1
AgneseBettio <bettio.agnese@gmail.com>
2021-04-19 15:55:40 +0200

    inizio ad impostare js - comando a btn

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@
 //quali imput ho per fare prezzo? name(nome necessario per ordinazione) ingredients (#cheese-add #tomato-add #egg-add #lettuce-add #mustard-add #ketchap-add) coupon (applico % sconto ad arrey codici inventati)
 
 //creo var generali utilizzabili in file - DOM estrapolo elementi
+//non è form! 
+//cheeckbox - ha element.value e element.checked
+//voglio utilizzare ciclo per non ripetere singoli elementi ingredienti
+var finalPriceBtn = document.getElementById("button");
+finalPriceBtn.addEventListener("click", function(){
+
+})
