60e29809cfa140309d83d937d91321dd06e519dc
AgneseBettio <bettio.agnese@gmail.com>
2021-04-19 16:14:28 +0200

    cambiato var ingredients con ingredientsList xme+chiara

--- js/script.js
+++ js/script.js
@@ -7,6 +7,20 @@
 //cheeckbox - ha element.value e element.checked
 //voglio utilizzare ciclo per non ripetere singoli elementi ingredienti
 var finalPriceBtn = document.getElementById("button");
+var burgerCustomersName = document.getElementById("name");
+// if(!burgerCustomersName.value){
+//     alert("per ordinare il tuo burger è necessario inventarsi un nome");
+// } LO LEVO PER NON AVERE SEMPRE BLOCCAT LA PAGINA - RICORDARSI DI INSERIRLO NUOVAMENTE.
+console.log(burgerCustomersName);
+var ingredientsList = document.querySelectorAll(".ingredient [type ='checkbox']");
+console.log(ingredients);
+var sommaDefault = 50;
 finalPriceBtn.addEventListener("click", function(){
+  //come fare per calcolare se un elemento è inserito o meno? setto somma iniziale a 50 - ricordo che serve ParseInt per .value
 
+  // ---> voglio fare un ciclo
+  // ingredients.value - è prezzo /// ingredients.checked booleano
+  for(var i = 0; i< ingredientsList.length; i++ ){
+      var ingredient = 
+  }
 })
