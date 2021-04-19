78f984f4bdbbfab139cfc5f4d72a02c1d8deab9c
AgneseBettio <bettio.agnese@gmail.com>
2021-04-19 15:42:53 +0200

    inizializzazione

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
+
+//creo var generali utilizzabili in file - DOM estrapolo elementi
